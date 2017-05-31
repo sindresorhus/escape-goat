52eb1f851ae88233444674c7270252cf849bd968
Federico Brigante <bfred-it@users.noreply.github.com>
2017-05-31 14:16:14 +0800

    Use multiple replacements instead of a single complex regex (#2)

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 'use strict';
 
-const escapes = new Map([
-	['&', '&amp;'],
-	['<', '&lt;'],
-	['>', '&gt;'],
-	['"', '&quot;'],
-	['\'', '&#39;']
-]);
+exports.escape = input => input
+	.replace(/&/g, '&amp;')
+	.replace(/"/g, '&quot;')
+	.replace(/'/g, '&#39;')
+	.replace(/</g, '&lt;')
+	.replace(/>/g, '&gt;');
 
-const unescapes = new Map([
-	['&amp;', '&'],
-	['&lt;', '<'],
-	['&gt;', '>'],
-	['&quot;', '"'],
-	['&#39;', '\'']
-]);
-
-exports.escape = input =>
-	input.replace(/[&<>"']/g, m => escapes.get(m) || m);
-
-exports.unescape = input =>
-	input.replace(/&(?:amp|lt|gt|quot|#39);/g, m => unescapes.get(m) || m);
+exports.unescape = input => input
+	.replace(/&amp;/g, '&')
+	.replace(/&quot;/g, '"')
+	.replace(/&#39;/g, '\'')
+	.replace(/&lt;/g, '<')
+	.replace(/&gt;/g, '>');
