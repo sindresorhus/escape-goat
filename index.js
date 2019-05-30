ee3349151564b5ed225136e9d2e48e55107d775f
Federico Brigante <github@bfred.it>
2019-05-30 17:04:07 +0800

    Move `String()` to `*Tag` methods

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-exports.htmlEscape = string => String(string)
+exports.htmlEscape = string => string
 	.replace(/&/g, '&amp;')
 	.replace(/"/g, '&quot;')
 	.replace(/'/g, '&#39;')
 	.replace(/</g, '&lt;')
 	.replace(/>/g, '&gt;');
 
-exports.htmlUnescape = htmlString => String(htmlString)
+exports.htmlUnescape = htmlString => htmlString
 	.replace(/&gt;/g, '>')
 	.replace(/&lt;/g, '<')
 	.replace(/&#39;/g, '\'')
@@ -17,7 +17,7 @@ exports.htmlUnescape = htmlString => String(htmlString)
 exports.htmlEscapeTag = (strings, ...values) => {
 	let output = strings[0];
 	for (let i = 0; i < values.length; i++) {
-		output = output + exports.htmlEscape(values[i]) + strings[i + 1];
+		output = output + exports.htmlEscape(String(values[i])) + strings[i + 1];
 	}
 
 	return output;
@@ -26,7 +26,7 @@ exports.htmlEscapeTag = (strings, ...values) => {
 exports.htmlUnescapeTag = (strings, ...values) => {
 	let output = strings[0];
 	for (let i = 0; i < values.length; i++) {
-		output = output + exports.htmlUnescape(values[i]) + strings[i + 1];
+		output = output + exports.htmlUnescape(String(values[i])) + strings[i + 1];
 	}
 
 	return output;
