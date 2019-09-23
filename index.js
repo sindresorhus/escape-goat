f0d5024baa9049a39c27620ed97b120bf1430ef1
NicoleEtLui <petit.martin125@gmail.com>
2019-09-23 11:18:08 +0200

    Handle `&#039;` in addition to `&#39;` (#14)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.htmlEscape = string => string
 exports.htmlUnescape = htmlString => htmlString
 	.replace(/&gt;/g, '>')
 	.replace(/&lt;/g, '<')
-	.replace(/&#39;/g, '\'')
+	.replace(/&#0?39;/g, '\'')
 	.replace(/&quot;/g, '"')
 	.replace(/&amp;/g, '&');
 
