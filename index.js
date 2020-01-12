fd8a30ca413e2714685f35cf9dce7e9c6004196d
Federico Brigante <github@bfred.it>
2020-01-13 00:47:10 +0700

    Merge tag functions into regular functions

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 'use strict';
 
-exports.htmlEscape = string => string
+const htmlEscape = string => string
 	.replace(/&/g, '&amp;')
 	.replace(/"/g, '&quot;')
 	.replace(/'/g, '&#39;')
 	.replace(/</g, '&lt;')
 	.replace(/>/g, '&gt;');
 
-exports.htmlUnescape = htmlString => htmlString
+const htmlUnescape = htmlString => htmlString
 	.replace(/&gt;/g, '>')
 	.replace(/&lt;/g, '<')
 	.replace(/&#0?39;/g, '\'')
 	.replace(/&quot;/g, '"')
 	.replace(/&amp;/g, '&');
 
-exports.htmlEscapeTag = (strings, ...values) => {
+exports.htmlEscape = (strings, ...values) => {
+	if (typeof strings === 'string') {
+		return htmlEscape(strings);
+	}
+
 	let output = strings[0];
 	for (let i = 0; i < values.length; i++) {
-		output = output + exports.htmlEscape(String(values[i])) + strings[i + 1];
+		output = output + htmlEscape(String(values[i])) + strings[i + 1];
 	}
 
 	return output;
 };
 
-exports.htmlUnescapeTag = (strings, ...values) => {
+exports.htmlUnescape = (strings, ...values) => {
+	if (typeof strings === 'string') {
+		return htmlUnescape(strings);
+	}
+
 	let output = strings[0];
 	for (let i = 0; i < values.length; i++) {
-		output = output + exports.htmlUnescape(String(values[i])) + strings[i + 1];
+		output = output + htmlUnescape(String(values[i])) + strings[i + 1];
 	}
 
 	return output;
