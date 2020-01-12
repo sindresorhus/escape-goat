2bba6566705a7bdecadf615e6634dbab7bc56936
Federico Brigante <github@bfred.it>
2020-01-13 00:47:53 +0700

    Update type tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,12 @@
 import {expectType} from 'tsd';
-import {htmlEscape, htmlUnescape, htmlEscapeTag, htmlUnescapeTag} from '.';
+import {htmlEscape, htmlUnescape} from '.';
 
 expectType<string>(htmlEscape('🦄 & 🐐'));
 expectType<string>(htmlUnescape('🦄 &amp; 🐐'));
 expectType<string>(htmlEscape('Hello <em>World</em>'));
 
 const url = 'https://sindresorhus.com?x="🦄"';
-expectType<string>(htmlEscapeTag`<a href="${url}">Unicorn</a>`);
+expectType<string>(htmlEscape`<a href="${url}">Unicorn</a>`);
 
 const escaped = '🦄 &amp; 🐐';
-expectType<string>(htmlUnescapeTag`unicorn and goat: ${escaped}`);
+expectType<string>(htmlUnescape`unicorn and goat: ${escaped}`);
