6a1a642f3ab2828d12bba5d0d48626e7d98a7eee
Sindre Sorhus <sindresorhus@gmail.com>
2021-09-13 20:54:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Multiple replace() calls are actually faster than using replacer functions #2
-// TODO: Use replaceAll when targeting Node v15+
+// TODO: Use `String#replaceAll` when targeting Node.js 16.
+// Multiple `.replace()` calls are actually faster than using replacer functions #2.
 const _htmlEscape = string => string
-	.replace(/&/g, '&amp;') // Must happen first or else it will escape other just-escaped characters
+	.replace(/&/g, '&amp;') // Must happen first or else it will escape other just-escaped characters.
 	.replace(/"/g, '&quot;')
 	.replace(/'/g, '&#39;')
 	.replace(/</g, '&lt;')
@@ -12,7 +12,7 @@ const _htmlUnescape = htmlString => htmlString
 	.replace(/&lt;/g, '<')
 	.replace(/&#0?39;/g, '\'')
 	.replace(/&quot;/g, '"')
-	.replace(/&amp;/g, '&'); // Must happen last or else it will unescape other characters in the wrong order
+	.replace(/&amp;/g, '&'); // Must happen last or else it will unescape other characters in the wrong order.
 
 export function htmlEscape(strings, ...values) {
 	if (typeof strings === 'string') {
